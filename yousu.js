0f0a2fa17c1b6051b1b6228579a8c132328f75d0
maplesora <admin@maplesora.moe>
2015-09-05 11:50:23 +0800

    index

---
+++ yousu.js
@@ -0,0 +1,5 @@
+﻿/* 调整#box的水平位置 */
+$('#box').css('left',$(window).width()/2-$('#box').width()/2);
+$(window).resize(function() {
+	$('#box').css('left',$(window).width()/2-$('#box').width()/2);
+}); 
