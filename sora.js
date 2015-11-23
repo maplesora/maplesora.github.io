cc1f6d39bb1952cbc867fc03d8876072bb5faf25
maplesora <admin@maplesora.moe>
2015-11-23 22:44:38 +0800

    update

--- sora.js
+++
@@ -1,6 +0,0 @@
-﻿/* 调整#box的水平位置 */
-$('#box').css('left',$(window).width()/2-$('#box').width()/2);
-$(window).resize(function() {
-	$('#box').css('left',$(window).width()/2-$('#box').width()/2);
-}); 
-$('#cnzz_stat_icon_1256394349').css('display','none');
